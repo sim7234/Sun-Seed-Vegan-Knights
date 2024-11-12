ad083d7fb3acca1752ac6419595f2fe33f51b547
sim7234 <simon.olander.tenwolde@hotmail.se>
2024-11-12 15:14:33 +0100

    Updated melee Combat and added Enemy Script Folder

--- Assets/Scripts/PlayerScripts/PlayerMeleeCombatPrototype.cs
+++ Assets/Scripts/PlayerScripts/PlayerMeleeCombatPrototype.cs
@@ -4,7 +4,9 @@ public class PlayerMeleeCombatPrototype : MonoBehaviour
 {
 
     bool inputR1 = false;
+    bool doesDamage = false;
     Animator animator;
+    
 
     private void Start()
     {
@@ -13,11 +15,28 @@ public class PlayerMeleeCombatPrototype : MonoBehaviour
 
     void Update()
     {
-       inputR1 = Input.GetKeyDown(KeyCode.Joystick1Button5);
+        if (Input.GetKeyDown(KeyCode.Joystick1Button5))
+        {
+            inputR1 = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Joystick1Button5))
+        {
+            inputR1 = false;
+        }
 
         if (inputR1 == true)
         {
             animator.SetTrigger("PressedR1");
+            doesDamage = true;
+        }
+        else
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+            {
+                doesDamage = false;
+                Debug.Log("Animation Idle");
+            }
         }
 
     }
