e708bd3c5465825c28501824e05aca6eca8b7030
Killenkevin <kormarkkevin@gmail.com>
2024-11-13 15:12:14 +0100

    new input system
    changed input system for player movement, rotation and weapons attack

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -14,14 +15,43 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private GameObject directionIndicator;
+    
+    public PlayerInputActions playerMovement;
+    public InputAction move;
 
+    Vector2 moveDirection = Vector2.zero;
+
+
+    private void Awake()
+    {
+         playerMovement = new PlayerInputActions();
+    }
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
+
+    private void OnEnable() 
+    {
+        move = playerMovement.Player.Move;
+        move.Enable();
+    }
+
+    private void OnDisable() 
+    {
+        move.Disable();
+    }
+
     private void Update()
     {
-        Vector2 vecloity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveDirection = move.ReadValue<Vector2>();
+
+        if (moveDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            directionIndicator.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+        /*Vector2 vecloity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         vecloity.Normalize();
         if (vecloity.magnitude > 0 && rb2d.velocity.magnitude <= maxSpeed)
         {
@@ -32,6 +62,12 @@ public class PlayerMovement : MonoBehaviour
         if(direction.magnitude > 0)
         {
             directionIndicator.transform.up = direction;
+        }*/
+    }
+        private void FixedUpdate()
+        {
+            rb2d.velocity = moveDirection * moveSpeed;
+            
         }
     }
-}
+
--- Assets/Scripts/SpecialWeapon.cs
+++ Assets/Scripts/SpecialWeapon.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SpecialWeapon : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class SpecialWeapon : MonoBehaviour
     private int specialWeaponAttacks = 5;
     private int attackCounter;
 
+    public InputAction fire;
+
     [SerializeField]
     private GameObject baseWeapon;
 
@@ -17,35 +20,62 @@ public class SpecialWeapon : MonoBehaviour
 
     public float attackCooldown;
 
+    private void Awake()
+    {
+        fire = new PlayerInputActions().Player.Fire; 
+    }
+
+    private void OnEnable()
+    {
+        fire.Enable();
+        fire.performed += Fire; 
+    }
+
+    private void OnDisable()
+    {
+        fire.Disable();
+        fire.performed -= Fire;
+    }
+
     private void Start()
     {
         attackCounter = 0;
     }
-    private void Update()
+
+    private void Fire(InputAction.CallbackContext context)
     {
-        if (attackCounter >= specialWeaponAttacks)
-        {
-            bigSword.SetActive(false);
-            baseWeapon.SetActive(true);
-        }
-        if (bigSword.activeSelf && Input.GetKeyDown(KeyCode.Joystick1Button5) && attackCooldown < 0)
+        if (bigSword.activeSelf && attackCooldown <= 0)
         {
             attackCounter++;
             bigSword.GetComponent<Animator>().SetTrigger("Attack");
             attackCooldown = 1.5f;
-
             Debug.Log("Big swing");
+
+            if (attackCounter >= specialWeaponAttacks)
+            {
+                bigSword.SetActive(false);
+                baseWeapon.SetActive(true);
+            }
         }
-        else if (weaponPickupsInRange.Count > 0 && Input.GetKeyDown(KeyCode.Joystick1Button5))
+        
+        else if (weaponPickupsInRange.Count > 0)
         {
-            attackCounter = 0;
+            attackCounter = 0; 
             bigSword.SetActive(true);
             baseWeapon.SetActive(false);
+
             Destroy(weaponPickupsInRange[0]);
             weaponPickupsInRange.Clear();
+
+            Debug.Log("Special weapon picked up!");
         }
+    }
+
+    private void Update()
+    {
         attackCooldown -= Time.deltaTime;
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<WeaponPickup>() != null)
@@ -53,6 +83,7 @@ public class SpecialWeapon : MonoBehaviour
             weaponPickupsInRange.Add(collision.gameObject);
         }
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.GetComponent<WeaponPickup>() != null)
