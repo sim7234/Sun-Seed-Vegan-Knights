c2ebc2e865171ebb85c7be47be3933afdaa1d1fb
sim7234 <simon.olander.tenwolde@hotmail.se>
2024-11-15 11:21:47 +0100

    Menu script no longer needs to be commented out :)

--- Assets/Scripts/Main Menu/Menu.cs
+++ Assets/Scripts/Main Menu/Menu.cs
@@ -5,7 +5,10 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
 
-    Button playButton 
+    Button playButton;
+    Button SelectPlayersButton;
+    Button QuitButton;
+
 
     // Start is called before the first frame update
     void Start()
