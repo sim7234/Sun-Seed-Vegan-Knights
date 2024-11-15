768b19605981838fd1f210bffd2e05aaa7a2c102
sim7234 <simon.olander.tenwolde@hotmail.se>
2024-11-15 11:19:07 +0100

    Added main menu

---
+++ Assets/Scripts/Main Menu/Menu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+
+    Button playButton 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
