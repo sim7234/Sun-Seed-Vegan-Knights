b70e085fb0be4f2c956fccf51fb6d98a12a2324b
TeodorDahlen <Teodor.dahlen@hotmail.com>
2024-11-20 17:03:34 +0100

    Move on attack, sword animations, sprite update

---
+++ Assets/MoveOnAttack.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnAttack : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+    private GameObject rotationObject;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    public void moveWhenAttacking(float strength)
+    {
+        rb.AddForce(rotationObject.transform.up * strength, ForceMode2D.Impulse);
+    }
+}
