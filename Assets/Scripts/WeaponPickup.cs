1c8ee7249f0b5cde65366b17efae653fd744887c
TeodorDahlen <Teodor.dahlen@hotmail.com>
2024-11-29 09:57:14 +0100

    Hub is cuter and more clear

--- Assets/Scripts/WeaponPickup.cs
+++ Assets/Scripts/WeaponPickup.cs
@@ -7,14 +7,41 @@ public class WeaponPickup : MonoBehaviour
     [SerializeField]
     public WeaponType type;
 
+    [SerializeField]
+    private GameObject weaponPickupText;
+
+    private List<GameObject> playersInRange = new List<GameObject>();
+
     public WeaponType GetWeaponType()
     {
         SaveData.Instance.seedType = type;
 
         return type;
     }
-}
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            playersInRange.Add(gameObject);
+        }
+        weaponPickupText.SetActive(true);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+     
+        if(playersInRange.Contains(collision.gameObject))
+        {
+            playersInRange.Remove(collision.gameObject);
+        }
+        if(playersInRange.Count <= 0)
+        {
+            weaponPickupText.SetActive(false);
+        }
+    }
+    
+}
 
 public enum WeaponType
 {
