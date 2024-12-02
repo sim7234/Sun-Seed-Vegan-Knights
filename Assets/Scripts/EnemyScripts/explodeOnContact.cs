cdcd36fa6f1e1687a10d8a39982a06510c403b31
Killenkevin <kormarkkevin@gmail.com>
2024-12-02 09:17:29 +0100

    meatball counts towards enemy kill, tutorial dialogue change

--- Assets/Scripts/EnemyScripts/explodeOnContact.cs
+++ Assets/Scripts/EnemyScripts/explodeOnContact.cs
@@ -3,24 +3,22 @@ using UnityEngine;
 public class explodeOnContact : MonoBehaviour
 {
     bool startExplosion;
-    
     float explosionTimer = 0.5f;
     float timeTillExplosion;
 
-    SpriteRenderer köttBulleSprite;
+    SpriteRenderer kottBulleSprite;
 
     [SerializeField] Collider2D explosionCollider;
     SpriteRenderer explosionSprite;
     EnemyAttacks enemyAttacksScript;
 
     float growthRate = 0.1f;
-    
 
     private void Start()
     {
         enemyAttacksScript = GetComponent<EnemyAttacks>();
         explosionSprite = explosionCollider.gameObject.GetComponent<SpriteRenderer>();
-        köttBulleSprite = GetComponent<SpriteRenderer>();
+        kottBulleSprite = GetComponent<SpriteRenderer>();
 
         timeTillExplosion = explosionTimer;
     }
@@ -40,21 +38,30 @@ public class explodeOnContact : MonoBehaviour
                 explosionSprite.enabled = true;
             }
 
-
-            köttBulleSprite.color = Color.red;
+            kottBulleSprite.color = Color.red;
             if (timeTillExplosion > 0)
             {
                 timeTillExplosion -= Time.deltaTime;
             }
 
-            transform.localScale = new Vector3(transform.localScale.x + Time.deltaTime * growthRate, transform.localScale.y + Time.deltaTime * growthRate);
-            
+            transform.localScale = new Vector3(
+                transform.localScale.x + Time.deltaTime * growthRate,
+                transform.localScale.y + Time.deltaTime * growthRate
+            );
 
             if (timeTillExplosion <= 0)
             {
+                NotifyMissionMaster();
                 Destroy(gameObject);
             }
+        }
+    }
 
+    private void NotifyMissionMaster()
+    {
+        if (MissionMaster.Instance != null)
+        {
+            MissionMaster.Instance.EnemyKilled(gameObject);
         }
     }
 }
