d7e8ad09350ae584da16e756664c34bf4e0aed80
Killenkevin <kormarkkevin@gmail.com>
2024-12-03 15:21:12 +0100

    tutorial updates, systems finished

--- Assets/Scripts/Tutorial Scripts/wallBreakWeapon.cs
+++ Assets/Scripts/Tutorial Scripts/wallBreakWeapon.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class WallBreakWeapon : MonoBehaviour
 {
+    [SerializeField] private GameObject[] objectsToActivate; 
+    [SerializeField] private float activationDelay = 5f; 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         SpecialWeapon specialWeapon = collision.GetComponent<SpecialWeapon>();
         if (specialWeapon != null && specialWeapon.IsWieldingSword())
         {
             specialWeapon.DisableSpecialWeapon();
+            StartCoroutine(ActivateObjectsAfterDelay());
+        }
+    }
+
+    private IEnumerator ActivateObjectsAfterDelay()
+    {
+        yield return new WaitForSeconds(activationDelay); 
+        
+        foreach (GameObject obj in objectsToActivate)
+        {
+            if (obj != null)
+            {
+                obj.SetActive(true); 
+            }
         }
     }
 }
