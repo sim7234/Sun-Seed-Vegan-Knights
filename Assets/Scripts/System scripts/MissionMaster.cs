31c478b66d237b70c1662e33045ac389dc4a6eeb
TeodorDahlen <Teodor.dahlen@hotmail.com>
2025-01-03 14:12:48 +0100

    Revert "Game over resets scene instead of teleporting you to a random location outside hub"
    This reverts commit d0376142a55b34fee8ebff0709375f3fbc816214.

--- Assets/Scripts/System scripts/MissionMaster.cs
+++ Assets/Scripts/System scripts/MissionMaster.cs
@@ -141,7 +141,7 @@ public class MissionMaster : MonoBehaviour
             }
             else
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                SceneManager.LoadScene("True Hub"); 
             }
         }
     }
